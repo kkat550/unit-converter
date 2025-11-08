fcefbb7133d78c6aa02a4636638b6afe04c2576d
kkat550 <223910872+kkat550@users.noreply.github.com>
2025-11-08 21:17:39 +0100

    Add relationships between changes of values

--- javascript.js
+++ javascript.js
@@ -1,51 +1,78 @@
-let quantityBox = document.getElementById("quantityId");
-quantityBox.addEventListener("input", printFunctionQ);
+console.log("Hi!");
 
-function printFunctionQ() {
-    let selectedOptionIndex = quantityBox.options[quantityBox.selectedIndex];
-    selectedOption = selectedOptionIndex.value;
-    let quantityOption = selectedOption;
-    // console.log(quantityOption);
-}
-
-let inputElement = document.getElementById("inputNumberId");
-inputElement.addEventListener("input", printFunctionIN);
-
-function printFunctionIN(inputElement) {
-    let inputValue = inputElement.target.value;
-    // console.log(inputValue);
-}
-
-let inputBox = document.getElementById("inputBoxId");
-inputBox.addEventListener("input", printFunctionIB);
-
-function printFunctionIB() {
-    let selectedOptionIndex = inputBox.options[inputBox.selectedIndex];
-    selectedOption = selectedOptionIndex.value;
-    let inputOption = selectedOption;
-    // console.log(inputOption);
-}
-
-let outputElement = document.getElementById("outputNumberId");
-outputElement.addEventListener("input", printFunctionON);
-
-function printFunctionON(outputElement) {
-    let outputValue = outputElement.target.value;
-    // console.log(outputValue);
-}
-
-let outputBox = document.getElementById("outputBoxId");
-outputBox.addEventListener("input", printFunctionOB);
-
-function printFunctionOB() {
-    let selectedOptionIndex = outputBox.options[outputBox.selectedIndex];
-    selectedOption = selectedOptionIndex.value;
-    let outputOption = selectedOption;
-    // console.log(outputOption);
-}
-
-// quantityOption -> the quantity
+// quantityOption -> the quantity, i.e. time, length...
 // inputValue -> the value of input
 // outputValue -> the value of output
 // inputOption -> the unit of input
 // outputOption -> the unit of output
+
+let quantityOption = null;
+let inputValue = null;
+let outputValue = null;
+let inputOption = null;
+let outputOption = null;
+
+let quantityBox = document.getElementById("quantityId");
+quantityBox.addEventListener("input", functionQuantity);
+
+function functionQuantity() {
+    let selectedOptionIndex = quantityBox.options[quantityBox.selectedIndex];
+    selectedOption = selectedOptionIndex.value;
+    quantityOption = selectedOption;
+    // console.log(quantityOption);
+}
+
+let inputElement = document.getElementById("inputNumberId");
+inputElement.addEventListener("input", functionInputNumber);
+
+function functionInputNumber(inputElement) {
+    inputValue = inputElement.target.value;
+    changeOutputValue(computeValue(inputValue, inputOption, outputOption));
+    // console.log(inputValue);
+}
+
+let inputBox = document.getElementById("inputBoxId");
+inputBox.addEventListener("input", functionInputBox);
+
+function functionInputBox() {
+    let selectedOptionIndex = inputBox.options[inputBox.selectedIndex];
+    selectedOption = selectedOptionIndex.value;
+    inputOption = selectedOption;
+    changeOutputValue(computeValue(inputValue, inputOption, outputOption));
+    // console.log(inputOption);
+}
+
+let outputElement = document.getElementById("outputNumberId");
+outputElement.addEventListener("input", functionOutputNumber);
+
+function functionOutputNumber(outputElement) {
+    outputValue = outputElement.target.value;
+    changeInputValue(computeValue(outputValue, outputOption, inputOption));
+    // console.log(outputValue);
+}
+
+let outputBox = document.getElementById("outputBoxId");
+outputBox.addEventListener("input", functionOutputBox);
+
+function functionOutputBox() {
+    let selectedOptionIndex = outputBox.options[outputBox.selectedIndex];
+    selectedOption = selectedOptionIndex.value;
+    outputOption = selectedOption;
+    changeInputValue(computeValue(outputValue, outputOption, inputOption));
+    // console.log(outputOption);
+}
+
+function changeInputValue(value) {
+    document.getElementById("inputNumberId").value = value;
+}
+
+function changeOutputValue(value) {
+    document.getElementById("outputNumberId").value = value;
+}
+
+function computeValue(startingValue, startingUnit, endUnit) {
+    if (startingValue == null || startingUnit == undefined || endUnit == null) {
+        return;
+    }
+    return 1;
+}
