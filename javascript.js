84f56b0752d6f953ab0530840a5483c563aee6ba
kkat550 <223910872+kkat550@users.noreply.github.com>
2025-11-08 19:04:54 +0100

    Add js for all user input

--- javascript.js
+++ javascript.js
@@ -3,7 +3,7 @@ console.log("hello");
 let quantityBox = document.getElementById("quantityId");
 quantityBox.addEventListener("input", printFunctionQ);
 
-function printFunctionQ(quantityElement) {
+function printFunctionQ() {
     let selectedOptionIndex = quantityBox.options[quantityBox.selectedIndex];
     selectedOption = selectedOptionIndex.value;
     console.log(selectedOption);
@@ -14,6 +14,31 @@ inputElement.addEventListener("input", printFunctionIN);
 
 function printFunctionIN(inputElement) {
     console.log(inputElement.target.value);
+}
+
+let inputBox = document.getElementById("inputBoxId");
+inputBox.addEventListener("input", printFunctionIB);
+
+function printFunctionIB() {
+    let selectedOptionIndex = inputBox.options[inputBox.selectedIndex];
+    selectedOption = selectedOptionIndex.value;
+    console.log(selectedOption);
+}
+
+let outputElement = document.getElementById("outputNumberId");
+outputElement.addEventListener("input", printFunctionON);
+
+function printFunctionON(outputElement) {
+    console.log(outputElement.target.value);
+}
+
+let outputBox = document.getElementById("outputBoxId");
+outputBox.addEventListener("input", printFunctionOB);
+
+function printFunctionOB() {
+    let selectedOptionIndex = outputBox.options[outputBox.selectedIndex];
+    selectedOption = selectedOptionIndex.value;
+    console.log(selectedOption);
 }
 
 // while (true) {
